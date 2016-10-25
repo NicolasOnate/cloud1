d17d65be19e209b8bf1168693bc5dc83102f98d9
Nicolas Alejandro Onate Onate <nionate@alumnos.ubiobio.cl>
2016-10-25 12:19:44 -0300

    Se completa codigo de index

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php
-	
-	echo "Hola mundo";
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>
+		<?= 'Hola mundo' ?>
+	</h1>
 
-?>
+	<?php 
+		for ($i=2; $i < 6 ; $i++) { 
+			echo "<h$i> Como estás? </h$i>";
+		}
+	?>
+</body>
+</html>
